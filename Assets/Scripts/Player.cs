9c706b2a3c7fd2c82ecf2cbde7d590e62f81fefc
Abhinay Thakur <a8h1nay@gmail.com>
2020-09-19 14:26:28 +0530

    Create player controller script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Storing player's input
+        float input = Input.GetAxisRaw("Horizontal");
+
+        // Moving player
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+    }
+}
