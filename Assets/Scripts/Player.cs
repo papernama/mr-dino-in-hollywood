337999536b8f9dae17994dc245484be769dcfa40
Abhinay Thakur <a8h1nay@gmail.com>
2020-09-20 01:47:20 +0530

    Define transitions to the animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody2D rb;
     public float speed;
+    private float input;
+
+    Rigidbody2D rb;
+    Animator anime;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+        anime = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    private void Update(){
+        bool flag = input != 0 ? true : false;
+        anime.SetBool ("isRunning", flag);
+    }
+
+    void FixedUpdate(){
         // Storing player's input
-        float input = Input.GetAxisRaw("Horizontal");
+        input = Input.GetAxisRaw("Horizontal");
 
         // Moving player
         rb.velocity = new Vector2(input * speed, rb.velocity.y);
